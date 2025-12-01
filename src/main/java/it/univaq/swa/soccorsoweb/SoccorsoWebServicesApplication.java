fdb34494cc264bd7a276ba6d9745017b0349a718
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-12-01 20:22:09 +0100

    setup progetto SpringBoot

---
+++ src/main/java/it/univaq/swa/soccorsoweb/SoccorsoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package it.univaq.swa.soccorsoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccorsoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccorsoWebServicesApplication.class, args);
+	}
+
+}
